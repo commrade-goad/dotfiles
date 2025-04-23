22ece80c96bddfe9671883e35b38420d180be184
Fernando Perry <fernandoperry1234@gmail.com>
2025-04-23 23:51:52 +0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# dotfiles
+### NOTE:
+all of the binary file in ``sway/script`` ``hypr/scripts`` ``waybar/scripts`` folder source code can be located on my github repository
+  - [goad-rust-batt-reminder](https://github.com/commrade-goad/rust-batt-reminder)
+  - [waybar-mpris](https://github.com/commrade-goad/waybar-mpris)
+  - [cusprus](https://github.com/commrade-goad/cusprus)
+  - [dunst-notif-history](https://github.com/commrade-goad/dunst-notif-history)
+  - [ruskey](https://github.com/commrade-goad/ruskey)
+  - [batt-watchdog](https://github.com/commrade-goad/batt-watchdog)
+  - [media-master](https://github.com/commrade-goad/media-master)
+  - [scwork](https://github.com/commrade-goad/scwork)
---
+++ config/nvim/lua/plug.lua
@@ -0,0 +1,66 @@
+return {
+    {"rhysd/clever-f.vim"},
+    {"sainnhe/everforest",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.everforest_enable_italic = true
+        end
+    },
+    {"nvim-telescope/telescope.nvim",
+        name = "telescope",
+        event= "VeryLazy"
+    },
+    {'nvim-lua/plenary.nvim', lazy = true},
+    {"folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = { preset = "helix", timeoutlen = 1500 },
+        keys = {
+            {"<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {'tpope/vim-fugitive', event = "VeryLazy"},
+    {"github/copilot.vim",
+        config = function()
+            require('plugconf.copilot')
+        end
+    },
+    -- {'windwp/nvim-autopairs', event = "InsertEnter", opts = {}},
+    -- {'mbbill/undotree',
+    --     event = "VeryLazy",
+    --     config = function ()
+    --         require('plugconf.undotree')
+    --     end
+    -- },
+    {'dhruvasagar/vim-table-mode', event = "InsertEnter"},
+
+    -- LSP and syntax
+    {'nvim-treesitter/nvim-treesitter',
+        event = "VeryLazy",
+        config = function()
+            require("plugconf.treesitter")
+        end
+    },
+    {'neovim/nvim-lspconfig'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp', event = "InsertEnter"},
+    {'hrsh7th/cmp-buffer'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-nvim-lua'},
+    {'FelipeLema/cmp-async-path'},
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip',
+        event = "InsertEnter",
+        config = function ()
+            require("plugconf.luasnip")
+        end
+    },
+}
